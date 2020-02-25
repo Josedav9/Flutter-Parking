7b4a53c1595e3f58c597f640648f21f7fbc4e23d
Jose Alvarez <jose.alvarez@lever.co>
2020-02-24 21:38:18 -0500

    Register

--- lib/widgets/register.dart
+++ lib/widgets/register.dart
@@ -1,15 +1,127 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class RegisterPage extends StatelessWidget {
+  void _submitForm() {}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Registro'),
-      ),
-      body: Center(
-        child: Text("Esta es la pagina de registro"),
-      ),
-    );
+        resizeToAvoidBottomPadding: false,
+        appBar: AppBar(
+          title: Text('Registro'),
+        ),
+        body: Container(
+          child: Column(
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.all(8.0),
+                decoration: BoxDecoration(
+                    border:
+                        Border(bottom: BorderSide(color: Colors.grey[100]))),
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: "Nombre de usuario",
+                    hintStyle: TextStyle(color: Colors.grey),
+                  ),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(8.0),
+                decoration: BoxDecoration(
+                    border:
+                        Border(bottom: BorderSide(color: Colors.grey[100]))),
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: "Password",
+                    hintStyle: TextStyle(color: Colors.grey),
+                  ),
+                  obscureText: true,
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(8.0),
+                decoration: BoxDecoration(
+                    border:
+                        Border(bottom: BorderSide(color: Colors.grey[100]))),
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: "Correo",
+                    hintStyle: TextStyle(color: Colors.grey),
+                  ),
+                  keyboardType: TextInputType.emailAddress,
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(8.0),
+                decoration: BoxDecoration(
+                    border:
+                        Border(bottom: BorderSide(color: Colors.grey[100]))),
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: "Numero de Telefono",
+                    hintStyle: TextStyle(color: Colors.grey),
+                  ),
+                  keyboardType: TextInputType.number,
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(8.0),
+                decoration: BoxDecoration(
+                    border:
+                        Border(bottom: BorderSide(color: Colors.grey[100]))),
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: "Nombre",
+                    hintStyle: TextStyle(color: Colors.grey),
+                  ),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(8.0),
+                decoration: BoxDecoration(
+                    border:
+                        Border(bottom: BorderSide(color: Colors.grey[100]))),
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: "Apellido",
+                    hintStyle: TextStyle(color: Colors.grey),
+                  ),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(8.0),
+                decoration: BoxDecoration(
+                    border:
+                        Border(bottom: BorderSide(color: Colors.grey[100]))),
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: "Codigo del barrio",
+                    hintStyle: TextStyle(color: Colors.grey),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              RaisedButton(
+                onPressed: _submitForm,
+                child: Text('Registrarse'),
+                color: Colors.blue,
+                textColor: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              )
+            ],
+          ),
+        ));
   }
 }
